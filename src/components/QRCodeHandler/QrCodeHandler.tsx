ccbf01c39aaa32d8ae35125ec86f11da14a296c4
karol.kosek <karol99kos@gmail.com>
2022-05-27 13:55:10 +0200

    Adjust QrBarCode scanner

--- src/components/QRCodeHandler/QrCodeHandler.tsx
+++ src/components/QRCodeHandler/QrCodeHandler.tsx
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 import {BarCodeScanner, BarCodeEvent} from 'expo-barcode-scanner';
 
 import tw from '@core/styles/tailwind';
-import { useNavigation } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 
 const getFlowerID = (dataFromQRCode: string): string => {
   return dataFromQRCode;
@@ -12,7 +12,9 @@ const getFlowerID = (dataFromQRCode: string): string => {
 
 const QRCodeHandler = () => {
   const [hasPermission, setHasPermission] = useState<boolean>();
-  const navigation = useNavigation();
+  const [renderBarCode, setRenderBarCode] = useState<boolean>();
+  const {navigate} = useNavigation();
+
   useEffect(() => {
     const handlePermission = async () => {
       const {status} = await BarCodeScanner.requestPermissionsAsync();
@@ -21,22 +23,30 @@ const QRCodeHandler = () => {
     handlePermission();
   }, []);
 
+  useFocusEffect(() => {
+    if(hasPermission){
+      setRenderBarCode(true);
+    }
+  })
+
   const handleBarCodeScanned = ({data}: BarCodeEvent) => {
-    console.log('Scanning ' + data);
+    setRenderBarCode(false);
     const flowerID = getFlowerID(data);
-    navigation.navigate('PlantDetails', { id: flowerID })
+    navigate('PlantDetails', { id: flowerID })
   };
 
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
   }
+
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
+
   return (
     <View style={tw`border-2 flex-1 justify-center items-center`}>
       <Text>Scan the flower</Text>
-      <BarCodeScanner style={tw`border-2 border-green w-60 h-4/6 m-4`} onBarCodeScanned={handleBarCodeScanned}/>
+      {renderBarCode && <BarCodeScanner style={tw`border-2 border-green w-60 h-4/6 m-4`} onBarCodeScanned={handleBarCodeScanned}/>}
     </View>
   );
 };
