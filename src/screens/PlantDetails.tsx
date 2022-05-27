e6e490a9cbfe0c21a3e07c576a5809afe51cbf45
karol.kosek <karol99kos@gmail.com>
2022-05-27 17:15:16 +0200

    Cleanup plant details

--- src/screens/PlantDetails.tsx
+++ src/screens/PlantDetails.tsx
@@ -1,28 +1,10 @@
 import React from 'react';
-// import PlantDetailsContainer from '@core/components/PlantDetailsContainer';
-import SunDemandIcon from '../../assets/sun-demand-icon.svg';
-import { View, Text } from 'react-native';
-import tw from '@core/styles/tailwind';
+
 import PlantDetailsContainer from '@core/components/PlantDetailsContainer';
 
 const PlantDetails = () => {
   return (
     <PlantDetailsContainer />
-//   <View style={tw`m-20`}>
-//   <View style={tw`flex-row justify-center items-center`}>
-//     <View>
-//       <SunDemandIcon height={36} width={36} />
-//     </View>
-//     <View>
-//       <Text style={tw`text-xl font-semibold w-50px`}>
-//         75%
-//       </Text>
-//       <Text style={tw`text-xs`}>
-//         (średnie nasłonecznienie)
-//       </Text>
-//     </View>
-//   </View>
-//  </View>
   );
 };
 
