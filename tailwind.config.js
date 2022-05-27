9b83c9d241896d4092c4cf994fae841b37cf627c
karolkosek <105663776+karolkosek@users.noreply.github.com>
2022-05-27 10:43:02 +0200

    Add tailwind with custom colors (#2)

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    theme: {
+        colors: {
+            ...colors,
+            'very-dark-green': '#2D452F',
+            'dark-green': '#6B9D4A',
+            green: '#6B9D4A',
+            'ligth-green': '#CFE2CE',
+            ligth: '#F1F1F1',
+        }
+    }
+}
