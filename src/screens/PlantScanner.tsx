2b2a3836676c53fc20518e64867388524aa35230
karol.kosek <karol99kos@gmail.com>
2022-05-27 10:47:08 +0200

    Do cleanup PlantScanner

--- src/screens/PlantScanner.tsx
+++ src/screens/PlantScanner.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
 import tw from '@core/styles/tailwind';
 
 const PlantScanner = () => {
@@ -10,12 +10,5 @@ const PlantScanner = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default PlantScanner;
